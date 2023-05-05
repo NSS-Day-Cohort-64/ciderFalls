272fe1bfd1576b53065e76c6e9d0f96b1fe65086
Jonathan VanDuyne <jonathanvanduyne@gmail.com>
2023-05-05 14:22:14 -0400

    first commit

---
+++ scripts/database.js
@@ -0,0 +1,23 @@
+const database = {
+    guests: [
+
+    ],
+    parkServices: [
+        { id: 1, service: "rafting" },
+        { id: 2, service: "canoeing" },
+        { id: 3, service: "fishing" },
+        { id: 4, service: "hiking" },
+        { id: 5, service: "picnicking" },
+        { id: 6, service: "rock climbing" },
+        { id: 7, service: "lodging" },
+        { id: 8, service: "parking" },
+        { id: 9, service: "information" },
+        { id: 10, service: "fishing" },
+        { id: 11, service: "rock climbing" },
+
+    ],
+
+    parkAreas: [
+        { id: 1, }
+    ]
+}
